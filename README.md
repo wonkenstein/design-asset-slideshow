99606ab4b1d2fe95955cb479afd22848a3292a53
mwong <mark.wong@tmsw.com>
2013-12-12 11:36:56 +0000

    Adding assets and updating files with comments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 design-asset-slideshow
 ======================
+
+This is a design asset slideshow.
+
+Put static images into the
--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>McDonald's Monopoly 2014</title>
-    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<title>Generic Title</title>
+
     <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
 
 <style type="text/css">
-body {background-color:#1C0125;}
-.flex-direction-nav a {background-color:#FFF; color:#FFF; height:28px; width:27px;}
+body {background-color:#333;} /* Set this to color of the images  */
+.container {width: 1024px;} /* Set this to the width of the images */
+.flex-direction-nav a {background-color:#FFF; color:#FFF; height:28px; width:27px;} /* For the navigation */
 
 </style>
 </head>
@@ -20,7 +22,8 @@ body {background-color:#1C0125;}
             <ul class="slides">
 
                 <?php
-                $dir = 'designs';
+                // files are put into alphabetical order
+                $dir = 'assets';
                 if ($handle = opendir($dir)) {
                     while (false !== ($entry = readdir($handle))) {
                         if (!preg_match('#^\..*#', $entry)) {
@@ -42,14 +45,13 @@ body {background-color:#1C0125;}
 </body>
 
 <!-- jQuery -->
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
 <!-- FlexSlider -->
 <script type="text/javascript" src="js/jquery.flexslider.js"></script>
 <script type="text/javascript">
 
     $(window).load(function() {
-
       $('.flexslider').flexslider({
 
 		slideshow: false,
